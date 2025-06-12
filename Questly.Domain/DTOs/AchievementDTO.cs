bab0c055dc940350d7603d82ef8a5bb46023ed82
ShdwKick <4d5.gamedev@gmail.com>
2025-06-12 17:29:46 +0300

    Add DTOs

---
+++ Questly.Domain/DTOs/AchievementDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DataModels.DTOs;
+
+public class AchievementDTO
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Goal { get; set; } = 1;
+    public int RewardScore { get; set; }
+    public Guid? CityId { get; set; }
+    public City City { get; set; }
+    public string IconUrl { get; set; }
+    public float? Lat { get; set; }
+    public float? Lon { get; set; }
+    public Guid? CategoryId { get; set; }
+    public AchievementCategory Category { get; set; }
+    public bool IsPartner { get; set; }
+}
---
+++ Questly.Domain/DTOs/CityDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DataModels.DTOs;
+
+public class CityDTO
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public double? Lat { get; set; }
+    public double? Lon { get; set; }
+}
